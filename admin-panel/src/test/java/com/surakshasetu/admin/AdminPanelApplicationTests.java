487c2eeb25b20708b56e4cef631abe597502676a
Nikul Ladumor <ladumornikul999@gmail.com>
2026-08-04 12:08:38 +0530

    Admin panel

---
+++ admin-panel/src/test/java/com/surakshasetu/admin/AdminPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surakshasetu.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminPanelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
