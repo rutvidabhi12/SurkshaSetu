dc5acbebe5e7157c57762039cf6bb0ba8b7936f0
bhanderivrushti16 <bhanderivrushti@gmail.com>
2026-08-09 21:38:01 +0530

    login page

--- app/src/main/java/com/example/surkshasetu/MainActivity.java
+++ app/src/main/java/com/example/surkshasetu/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.surkshasetu;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +17,39 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_login);
 
+
         Window window = getWindow();
         window.setStatusBarColor(Color.TRANSPARENT);
+
+        EditText etUsername = findViewById(R.id.etUsername);
+        EditText etPassword = findViewById(R.id.etPassword);
+
+        MaterialButton btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(v -> {
+
+            String username = etUsername.getText().toString().trim();
+            String password = etPassword.getText().toString().trim();
+
+           if (username.isEmpty()) {
+                etUsername.setError("Enter User ID / Email");
+                etUsername.requestFocus();
+                return;
+            }
+
+
+            if (password.isEmpty()) {
+                etPassword.setError("Enter Password");
+                etPassword.requestFocus();
+                return;
+            }
+
+
+            android.widget.Toast.makeText(
+                    MainActivity.this,
+                    "Login successful",
+                    android.widget.Toast.LENGTH_SHORT
+            ).show();
+        });
     }
 }
