479b1a2216498e2a7828dd5041700f35042ce808
bhanderivrushti16 <bhanderivrushti@gmail.com>
2026-08-09 21:21:43 +0530

    login page

--- app/src/main/java/com/example/surkshasetu/MainActivity.java
+++ app/src/main/java/com/example/surkshasetu/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.surkshasetu;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_login);
+
+        Window window = getWindow();
+        window.setStatusBarColor(Color.TRANSPARENT);
     }
 }
